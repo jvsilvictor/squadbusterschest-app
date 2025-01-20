cd003e7a2c516d9ae9b8e2054f9af4a7a0f6189a
jvsilvictor <33354434+jvsilvictor@users.noreply.github.com>
2025-01-20 00:06:58 +0000

    v0.0.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,32 @@
 import streamlit as st
 
+# _chets_lsit = st.secrets['valor_ref']
+
+_chets_lsit = [1,2]
+
+def getNextChestName(index):
+    if _chets_lsit[index] == 0:
+        return "Common"
+    elif _chets_lsit[index] == 1:
+        return "Rare"
+    elif _chets_lsit[index] == 2:
+        return "Epic"
+    else:
+        return  "Error! :c"
+
+def getNextIndex(batalha):
+    valor_ref = 3484
+    index_ref = 42
+    
+    if batalha >= valor_ref:
+        index = index_ref + (batalha % valor_ref)
+    else:
+        index = index_ref - (valor_ref - batalha)
+
+    index = (index + 1) % 60  # Incrementa o índice e aplica o limite 0-59
+    return index
+
+
 st.title("SQUAD BUSTERS - MY NEXT CHEST!")
 st.write(
     "Use seus multiplicadores de baú com mais sabedoria!"
@@ -7,3 +34,4 @@ st.write(
 st.write(
     "Use your chest multipliers more wisely!"
 )
+st.write(_chets_lsit)
